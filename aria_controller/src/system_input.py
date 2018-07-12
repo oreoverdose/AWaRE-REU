7e9364dae73347b1ea61515d0251e1991867c55c
Marina Malone <mmalon15@nd.edu>
2018-07-12 04:16:59 -0400

    finished aria controller, implement aria_ros, make rotation speed faster

---
+++ aria_controller/src/system_input.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import numpy as np
+import math
+import tf
+from std_msgs.msg import Float32MultiArray,Bool
+from geometry_msgs.msg import Twist
+from control_lift import GripperControl
+
+class systemInput:
+	
+	def __init__(self):
+		
+		self.sys_min_traj_sub = rospy.Subscriber("sysOut_min_traj",Float32MultiArray,self.sysMinTrajCallback)
+		
+		self.done_sub = rospy.Subscriber("done",Bool,self.doneCallback)
+		
+		self.cmd_vel_pub = rospy.Publisher("/rosaria/cmd_vel",Twist,queue_size=10)
+		
+		self.sysArray_sub = rospy.Subscriber("sysArray",Float32MultiArray,self.sysCallback)
+		
+		self.sysArray=0
+		
+		self.prevV = 0.0884
+		
+		
+	def doneCallback(self,done):
+		if done:
+			rospy.sleep(0.1)
+			pub = Twist()
+			pub.linear.x = 0
+			pub.angular.z = 0
+			self.cmd_vel_pub.publish(pub)
+			rospy.sleep(0.1)
+			gc = GripperControl()
+			gc.raise_lift()
+	
+	def sysCallback(self,array):
+		self.sysArray = array.data
+		
+		
+	def sysMinTrajCallback(self,sysMinTraj):
+		v=0
+		w=0
+		if len(sysMinTraj.data)==2:
+			sysMinTraj = np.array([[sysMinTraj.data[0]],[sysMinTraj.data[1]]])
+			rK = np.array([-0.6613,-1.3266])
+			alpha = rK.dot(sysMinTraj)
+			thetom1 = np.array([[1, 0.5],[0,1]]).dot(sysMinTraj)
+			print()
+			thetom2 = np.array([[0.125],[0.5]])*(alpha)
+			print(thetom2)
+			thetom = thetom1+thetom2
+			print("thetom")
+			print(thetom)
+			w=thetom[1]
+		if len(sysMinTraj.data)==4:
+			mK = np.array([
+				[-1.1973,0,-1.5931,0],
+				[0,-1.1973,0,-1.5931]])
+			sysMinTrajarr = np.asarray(list(sysMinTraj.data))
+			virtualControl =mK.dot(sysMinTrajarr)
+		
+			#print("===3 [u1,u2]===")
+			#print(virtualControl)
+		
+			#print("--cos(theta),sin(theta),velocity--")
+			print("--velocity--")
+			print(self.sysArray[2])
+			
+			u1 = virtualControl[0]
+			u2 = virtualControl[1]
+			cos = self.sysArray[0]
+			sin = self.sysArray[1]
+			v = self.sysArray[2]
+		
+			if -0.001<u2<-0.001:
+				u2 = 0
+			a = u1*cos + u2*sin
+	
+			if -0.0884<v<0.0884:
+				v = 0.0884
+			w = -u1*sin/v + u2*cos/v
+		
+			#print('===4. [a,w]===')
+			#print([a,w])
+			v = a*0.5 + self.prevV
+		
+			if v>1.4:
+				v=1.4
+			self.prevV = v
+			if w>np.deg2rad(300):
+				w=np.deg2rad(300)
+			if w<np.deg2rad(-300):
+				w=np.deg2rad(-300)
+		
+		pub = Twist()
+		pub.linear.x = v
+		pub.angular.z = -w
+		print("===5. publish Twist ===")
+		print(pub)
+		self.cmd_vel_pub.publish(pub)
+		
+
+def main(args):
+
+	rospy.init_node("sys_input",anonymous=True)
+	si = systemInput()
+	rospy.spin()
+	
+if __name__ == '__main__':
+	main(sys.argv)
---
+++ aria_controller/src/system_output_ARIA.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import numpy as np
+import geometry_msgs.msg
+import tf
+import math
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32MultiArray,Bool,Float32
+
+
+class systemOutput:
+
+	def __init__(self):
+
+		self.obj_pos = [0.3,-0.46]#[0.2,-0.512]#[0.1,0.54]#[0,-0.55]#[-0.1,0.54]#[-0.2,-0.512]#[0.55,0.229]#[0.55,0.]#[0.4,-0.377]#
+		self.robo_rel_pos = 0
+		self.gotRoboRelPos = False
+		
+		self.obj_found_sub = rospy.Subscriber("obj_found",Bool,self.foundCallback)
+		
+		self.odom_pub = rospy.Publisher("system_output",Float32MultiArray,queue_size = 10)
+		
+		self.done_sub = rospy.Subscriber("done",Bool,self.doneCallback)
+		
+		#get time... publish every 0.1 seconds
+		self.time_log = rospy.get_time()
+		
+		#offset a couple of mm because gripper distance
+		self.x = -0.55
+		self.y = 0 
+		
+		self.theta = 0
+		
+	
+	def doneCallback(self,done):
+		rospy.signal_shutdown("doneeee!")
+	
+	def foundCallback(self, found):
+		#get information from odom
+		self.alpha = 0
+		self.transform = 0
+		self.odom_sub = rospy.Subscriber("/rosaria/pose",Odometry,self.odomCallback)
+		self.theta_pub = rospy.Publisher("theta",Float32,queue_size = 10)
+		self.sysArray_pub = rospy.Publisher("sysArray",Float32MultiArray,queue_size=10)
+	
+	def odomCallback(self, odom):
+		position = odom.pose.pose.position
+		orientation = odom.pose.pose.orientation
+		
+		
+		if not self.gotRoboRelPos:
+			#position where robot found object
+			self.robo_rel_pos =[position.x,position.y]
+			print("---robot starting position---")
+			print(self.robo_rel_pos)
+			self.gotRoboRelPos = True
+			#angle between robo-world axis and object to robot axis
+			self.alpha = math.atan2(self.obj_pos[1]-self.robo_rel_pos[1],self.obj_pos[0]-self.robo_rel_pos[0])
+			print("---alpha---")
+			print(self.alpha)
+			self.transform = [
+				[np.cos(self.alpha),np.sin(self.alpha)],
+				[-np.sin(self.alpha),np.cos(self.alpha)]]
+			##############FIX##############
+			quaternion = (
+				orientation.x,
+				orientation.y,
+				orientation.z,
+				orientation.w)
+			euler = tf.transformations.euler_from_quaternion(quaternion)
+
+			#z-angle according to robot
+			self.init_yaw = euler[2]
+			################################
+			
+			
+			
+			self.theta = self.alpha
+			self.theta_pub.publish(self.theta)
+
+			rospy.sleep(2.5)
+		
+		###############FIX################
+		quaternion = (
+				orientation.x,
+				orientation.y,
+				orientation.z,
+				orientation.w)
+		euler = tf.transformations.euler_from_quaternion(quaternion)
+		#z-angle according to robot
+		yaw = euler[2]+self.init_yaw
+		#################################
+		
+		
+		self.now = rospy.get_time()
+		if(self.now - self.time_log >= 0.5):
+			print("---yaw---")
+			print(yaw)
+			self.theta = self.alpha - yaw
+			vel = odom.twist.twist.linear.x
+			self.sysArray_pub.publish(data=[np.cos(self.theta),np.sin(self.theta),vel])
+			self.theta_pub.publish(self.theta)
+			
+			
+			print("---real position---")
+			print(position.x,position.y)
+			
+			#position in orginal axis relative to object
+			orig_rel_pos = [[position.x-self.obj_pos[0]],[position.y-self.obj_pos[1]]]
+			#get position in rotated axis relative to object
+			prime_pos = np.dot(self.transform,orig_rel_pos)
+			
+			output = [
+				prime_pos[0][0],
+				prime_pos[1][0],
+				self.theta,
+				vel,
+				odom.twist.twist.linear.z]
+			print("======================================")
+			print("===1 (x,y,theta,vel,rotVel===")
+			print(output)
+			self.odom_pub.publish(data = output)
+			self.time_log = rospy.get_time()
+
+def main(args):
+	rospy.init_node("system_output",anonymous=True,disable_signals=True)
+	so = systemOutput()
+	
+	rospy.spin()
+	
+if __name__ == '__main__':
+	main(sys.argv)
